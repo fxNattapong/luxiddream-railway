d2fead7b78f1416ec627a748c9a64efd91f9cb42
Nattapong <nattapong.paungpool@gmail.com>
2024-03-16 22:04:21 +0700

    change config css

--- resources/views/webpages/layouts/Layout.blade.php
+++ resources/views/webpages/layouts/Layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/app.css') }}" media="all" rel="stylesheet" type="text/css" />
 
         <title>Layout</title>
     </head>
