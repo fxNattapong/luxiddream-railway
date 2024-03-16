f10105346d90c861254ce231643c50fc7bc5120f
Nattapong <nattapong.paungpool@gmail.com>
2024-03-17 00:42:32 +0700

    fix route secure https

--- routes/web.php
+++ routes/web.php
@@ -13,86 +13,86 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', "WebpagesController@HomePage")->Name('HomePage')->forceScheme('https');
-Route::get('/about', "WebpagesController@AboutPage")->Name('AboutPage')->forceScheme('https');
-Route::get('/account', "WebpagesController@AccountPage")->Name('AccountPage')->forceScheme('https');
-Route::get('/login', "WebpagesController@LoginPage")->Name('LoginPage')->forceScheme('https');
-Route::get('/register', "WebpagesController@RegisterPage")->Name('RegisterPage')->forceScheme('https');
-Route::get('/rule', "WebpagesController@RulePage")->Name('RulePage')->forceScheme('https');
+Route::get('/', "WebpagesController@HomePage")->Name('HomePage')->https();
+Route::get('/about', "WebpagesController@AboutPage")->Name('AboutPage')->https();
+Route::get('/account', "WebpagesController@AccountPage")->Name('AccountPage')->https();
+Route::get('/login', "WebpagesController@LoginPage")->Name('LoginPage')->https();
+Route::get('/register', "WebpagesController@RegisterPage")->Name('RegisterPage')->https();
+Route::get('/rule', "WebpagesController@RulePage")->Name('RulePage')->https();
 
 Route::prefix('admin')->group(function () {
-    Route::get('/', "AdminController@IndexRedirect")->Name('IndexRedirect')->forceScheme('https');
+    Route::get('/', "AdminController@IndexRedirect")->Name('IndexRedirect')->https();
 
-    Route::post('/fetch/account', "AdminController@FetchAccountData")->Name('FetchAccountData')->forceScheme('https');
-    Route::post('/account/edit', "AdminController@SubmitAccountEdit")->Name('SubmitAccountEdit')->forceScheme('https');
+    Route::post('/fetch/account', "AdminController@FetchAccountData")->Name('FetchAccountData')->https();
+    Route::post('/account/edit', "AdminController@SubmitAccountEdit")->Name('SubmitAccountEdit')->https();
 
-    Route::get('/players', "AdminController@Players")->Name('Players')->forceScheme('https');
-    Route::post('/player/add', "AdminController@SubmitPlayerAdd")->Name('SubmitPlayerAdd')->forceScheme('https');
-    Route::post('/player/edit', "AdminController@SubmitPlayerEdit")->Name('SubmitPlayerEdit')->forceScheme('https');
-    Route::post('/player/delete', "AdminController@SubmitPlayerDelete")->Name('SubmitPlayerDelete')->forceScheme('https');
+    Route::get('/players', "AdminController@Players")->Name('Players')->https();
+    Route::post('/player/add', "AdminController@SubmitPlayerAdd")->Name('SubmitPlayerAdd')->https();
+    Route::post('/player/edit', "AdminController@SubmitPlayerEdit")->Name('SubmitPlayerEdit')->https();
+    Route::post('/player/delete', "AdminController@SubmitPlayerDelete")->Name('SubmitPlayerDelete')->https();
     
-    Route::get('/players/rule', "AdminController@PlayersRule")->Name('PlayersRule')->forceScheme('https');
-    Route::post('/player/rule/add', "AdminController@SubmitPlayerRuleAdd")->Name('SubmitPlayerRuleAdd')->forceScheme('https');
-    Route::post('/player/rule/edit', "AdminController@SubmitPlayerRuleEdit")->Name('SubmitPlayerRuleEdit')->forceScheme('https');
-    Route::post('/player/rule/delete', "AdminController@SubmitPlayerRuleDelete")->Name('SubmitPlayerRuleDelete')->forceScheme('https');
+    Route::get('/players/rule', "AdminController@PlayersRule")->Name('PlayersRule')->https();
+    Route::post('/player/rule/add', "AdminController@SubmitPlayerRuleAdd")->Name('SubmitPlayerRuleAdd')->https();
+    Route::post('/player/rule/edit', "AdminController@SubmitPlayerRuleEdit")->Name('SubmitPlayerRuleEdit')->https();
+    Route::post('/player/rule/delete', "AdminController@SubmitPlayerRuleDelete")->Name('SubmitPlayerRuleDelete')->https();
 
-    Route::get('/levels', "AdminController@Levels")->Name('Levels')->forceScheme('https');
-    Route::post('/level/add', "AdminController@SubmitLevelAdd")->Name('SubmitLevelAdd')->forceScheme('https');
-    Route::post('/level/edit', "AdminController@SubmitLevelEdit")->Name('SubmitLevelEdit')->forceScheme('https');
-    Route::post('/level/delete', "AdminController@SubmitLevelDelete")->Name('SubmitLevelDelete')->forceScheme('https');
+    Route::get('/levels', "AdminController@Levels")->Name('Levels')->https();
+    Route::post('/level/add', "AdminController@SubmitLevelAdd")->Name('SubmitLevelAdd')->https();
+    Route::post('/level/edit', "AdminController@SubmitLevelEdit")->Name('SubmitLevelEdit')->https();
+    Route::post('/level/delete', "AdminController@SubmitLevelDelete")->Name('SubmitLevelDelete')->https();
 
-    Route::get('/nightmares', "AdminController@Nightmares")->Name('Nightmares')->forceScheme('https');
-    Route::post('/nightmare/add', "AdminController@SubmitNightmareAdd")->Name('SubmitNightmareAdd')->forceScheme('https');
-    Route::post('/nightmare/edit', "AdminController@SubmitNightmareEdit")->Name('SubmitNightmareEdit')->forceScheme('https');
-    Route::post('/nightmare/delete', "AdminController@SubmitNightmareDelete")->Name('SubmitNightmareDelete')->forceScheme('https');
+    Route::get('/nightmares', "AdminController@Nightmares")->Name('Nightmares')->https();
+    Route::post('/nightmare/add', "AdminController@SubmitNightmareAdd")->Name('SubmitNightmareAdd')->https();
+    Route::post('/nightmare/edit', "AdminController@SubmitNightmareEdit")->Name('SubmitNightmareEdit')->https();
+    Route::post('/nightmare/delete', "AdminController@SubmitNightmareDelete")->Name('SubmitNightmareDelete')->https();
 
-    Route::get('/links', "AdminController@Links")->Name('Links')->forceScheme('https');
-    Route::post('/link/add', "AdminController@SubmitLinkAdd")->Name('SubmitLinkAdd')->forceScheme('https');
-    Route::post('/link/edit', "AdminController@SubmitLinkEdit")->Name('SubmitLinkEdit')->forceScheme('https');
-    Route::post('/link/delete', "AdminController@SubmitLinkDelete")->Name('SubmitLinkDelete')->forceScheme('https');
+    Route::get('/links', "AdminController@Links")->Name('Links')->https();
+    Route::post('/link/add', "AdminController@SubmitLinkAdd")->Name('SubmitLinkAdd')->https();
+    Route::post('/link/edit', "AdminController@SubmitLinkEdit")->Name('SubmitLinkEdit')->https();
+    Route::post('/link/delete', "AdminController@SubmitLinkDelete")->Name('SubmitLinkDelete')->https();
 
-    Route::get('/cards', "AdminController@Cards")->Name('Cards')->forceScheme('https');
-    Route::post('/card/add', "AdminController@SubmitCardAdd")->Name('SubmitCardAdd')->forceScheme('https');
-    Route::post('/card/edit', "AdminController@SubmitCardEdit")->Name('SubmitCardEdit')->forceScheme('https');
-    Route::post('/card/delete', "AdminController@SubmitCardDelete")->Name('SubmitCardDelete')->forceScheme('https');
+    Route::get('/cards', "AdminController@Cards")->Name('Cards')->https();
+    Route::post('/card/add', "AdminController@SubmitCardAdd")->Name('SubmitCardAdd')->https();
+    Route::post('/card/edit', "AdminController@SubmitCardEdit")->Name('SubmitCardEdit')->https();
+    Route::post('/card/delete', "AdminController@SubmitCardDelete")->Name('SubmitCardDelete')->https();
 });
 
 Route::prefix('game')->group(function () {
-    Route::get('/', "GameController@Home")->Name('Home')->forceScheme('https');
-    Route::post('/register/process', "GameController@RegisterProcess")->Name('RegisterProcess')->forceScheme('https');
-    Route::post('/login/process', "GameController@LoginProcess")->Name('LoginProcess')->forceScheme('https');
-    Route::get('/logout', "GameController@Logout")->Name('Logout')->forceScheme('https');
+    Route::get('/', "GameController@Home")->Name('Home')->https();
+    Route::post('/register/process', "GameController@RegisterProcess")->Name('RegisterProcess')->https();
+    Route::post('/login/process', "GameController@LoginProcess")->Name('LoginProcess')->https();
+    Route::get('/logout', "GameController@Logout")->Name('Logout')->https();
     
     Route::prefix('room')->group(function () {
-        Route::post('/create', "GameController@RoomCreate")->Name('RoomCreate')->forceScheme('https');
-        Route::post('/join', "GameController@RoomJoin")->Name('RoomJoin')->forceScheme('https');
+        Route::post('/create', "GameController@RoomCreate")->Name('RoomCreate')->https();
+        Route::post('/join', "GameController@RoomJoin")->Name('RoomJoin')->https();
     
-        Route::get('/waiting', "GameController@RoomWaiting")->Name('RoomWaiting')->middleware('redirectIfAuth')->forceScheme('https');
-        Route::post('/leave/wating', "GameController@LeaveRoomWating")->Name('LeaveRoomWating')->forceScheme('https');
-        Route::post('/player/remove', "GameController@PlayerRemove")->Name('PlayerRemove')->forceScheme('https');
-        Route::post('/delete', "GameController@RoomDelete")->Name('RoomDelete')->forceScheme('https');
-        Route::post('/poll/players', "GameController@pollPlayers")->Name('pollPlayers')->forceScheme('https');
+        Route::get('/waiting', "GameController@RoomWaiting")->Name('RoomWaiting')->middleware('redirectIfAuth')->https();
+        Route::post('/leave/wating', "GameController@LeaveRoomWating")->Name('LeaveRoomWating')->https();
+        Route::post('/player/remove', "GameController@PlayerRemove")->Name('PlayerRemove')->https();
+        Route::post('/delete', "GameController@RoomDelete")->Name('RoomDelete')->https();
+        Route::post('/poll/players', "GameController@pollPlayers")->Name('pollPlayers')->https();
     
-        Route::post('/change/status', "GameController@ChangeStatus")->Name('ChangeStatus')->forceScheme('https');
+        Route::post('/change/status', "GameController@ChangeStatus")->Name('ChangeStatus')->https();
 
-        Route::post('/disconnect', "GameController@RoomDisconnect")->Name('RoomDisconnect')->forceScheme('https');
+        Route::post('/disconnect', "GameController@RoomDisconnect")->Name('RoomDisconnect')->https();
     
-        Route::post('/start', "GameController@StartGame")->Name('StartGame')->forceScheme('https');
-        Route::get('/start', "GameController@RoomPlay")->Name('RoomPlay')->middleware('redirectIfAuth')->forceScheme('https');
+        Route::post('/start', "GameController@StartGame")->Name('StartGame')->https();
+        Route::get('/start', "GameController@RoomPlay")->Name('RoomPlay')->middleware('redirectIfAuth')->https();
     
-        Route::post('/leave/playing', "GameController@LeaveRoom")->Name('LeaveRoom')->forceScheme('https');
-        Route::post('/start/timer', "GameController@StartTimer")->Name('StartTimer')->forceScheme('https');
-        Route::post('/end/timer', "GameController@EndTimer")->Name('EndTimer')->forceScheme('https');
-        Route::post('/poll/links', "GameController@PollLinks")->Name('PollLinks')->forceScheme('https');
-        Route::post('/fetch/timeout', "GameController@FetchTimeout")->Name('FetchTimeout')->forceScheme('https');
-        Route::post('/fetch/cards', "GameController@FetchCards")->Name('FetchCards')->forceScheme('https');
-        Route::post('/fetch/results', "GameController@FetchResults")->Name('FetchResults')->forceScheme('https');
-        Route::post('/round/card/add', "GameController@CardAdd")->Name('CardAdd')->forceScheme('https');
-        Route::post('/check/nightmare/link', "GameController@CheckNightmareLink")->Name('CheckNightmareLink')->forceScheme('https');
-        Route::post('/start/next/round', "GameController@StartNextRound")->Name('StartNextRound')->forceScheme('https');
-        Route::post('/start/next/circle', "GameController@StartNextCircle")->Name('StartNextCircle')->forceScheme('https');
-        Route::post('/end', "GameController@GameEnd")->Name('GameEnd')->forceScheme('https');
-        Route::post('/update/stats', "GameController@UpdateStats")->Name('UpdateStats')->forceScheme('https');
+        Route::post('/leave/playing', "GameController@LeaveRoom")->Name('LeaveRoom')->https();
+        Route::post('/start/timer', "GameController@StartTimer")->Name('StartTimer')->https();
+        Route::post('/end/timer', "GameController@EndTimer")->Name('EndTimer')->https();
+        Route::post('/poll/links', "GameController@PollLinks")->Name('PollLinks')->https();
+        Route::post('/fetch/timeout', "GameController@FetchTimeout")->Name('FetchTimeout')->https();
+        Route::post('/fetch/cards', "GameController@FetchCards")->Name('FetchCards')->https();
+        Route::post('/fetch/results', "GameController@FetchResults")->Name('FetchResults')->https();
+        Route::post('/round/card/add', "GameController@CardAdd")->Name('CardAdd')->https();
+        Route::post('/check/nightmare/link', "GameController@CheckNightmareLink")->Name('CheckNightmareLink')->https();
+        Route::post('/start/next/round', "GameController@StartNextRound")->Name('StartNextRound')->https();
+        Route::post('/start/next/circle', "GameController@StartNextCircle")->Name('StartNextCircle')->https();
+        Route::post('/end', "GameController@GameEnd")->Name('GameEnd')->https();
+        Route::post('/update/stats', "GameController@UpdateStats")->Name('UpdateStats')->https();
     });
     
 });
